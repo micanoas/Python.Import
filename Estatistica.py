e02cba0a28cc595c7702002f62edbf4ce1545d00
Michelle Canoas <163908232+micanoas@users.noreply.github.com>
2024-04-07 16:34:14 -0300

    Add files via upload

---
+++ Estatistica.py
@@ -0,0 +1,5 @@
+from teste import Computador
+computador1 = Computador("Michelle", "35 anos", "123.456.789-10", "131454", "R$ 100.000", "360 meses", "PF", "8 a.a", "R$ 1.000", "R$ 80.000" )
+print(computador1.nome, computador1.nascimento, computador1.identificador, computador1.contrato, computador1.valor, computador1.prazo, computador1.cliente, computador1.juros, computador1.prestacao, computador1.saldo_devedor)
+
+
---
+++ teste.py
@@ -0,0 +1,18 @@
+class Computador:
+    def __init__(self, nome, nascimento, identificador, contrato, valor, prazo, cliente, juros, prestacao, saldo):
+        self.nome = nome
+        self.nascimento = nascimento
+        self.identificador = identificador
+        self.contrato = contrato
+        self.valor = valor
+        self.prazo = prazo
+        self.cliente = cliente
+        self.juros = juros
+        self.prestacao = prestacao
+        self.saldo_devedor = saldo
+
+pass
+
+
+
+
